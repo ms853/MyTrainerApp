5e3850a4bf664600cd79e8c4ee10a8290b6754d8
unknown <msakyiama51@gmail.com>
2020-05-04 22:52:25 +0100

    intial commit

---
+++ constants/Colors.js
@@ -0,0 +1,15 @@
+//Colour set to change.
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
---
+++ src/screens/Auth/Signup.js
@@ -0,0 +1,16 @@
+import React,{ Component } from "react";
+import { Text, View, ScrollView } from "react-native";
+
+class Signup extends Component {
+    state = {}
+
+    render() {
+        return (
+            <View>
+                <Text>My Signup page.</Text>
+            </View>
+        );
+    }
+}
+
+export default Signup;
